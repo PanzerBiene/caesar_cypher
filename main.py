29d3ae4b8d08a415cd4860a13a62082ff08e23fb
Brandon Graupner <brandon.graupner@gmail.com>
2021-06-29 22:33:19 +1000

    v2 returns spaces

--- main.py
+++ main.py
@@ -10,28 +10,33 @@ def read_letters(file_name,c):
         letter = contents[i]
         output.append(caeser_cypher(letter, cypher))
         i += 1
-    return output
+    return [i for i in output]
 def create_cypher(c):
     cypher = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
               "v", "w", "x", "y", "z"]
+
     if c > 0:
-        while c >= 0:
+        while c > 0:
             a = cypher.pop(0)
             cypher.append(a)
             c -= 1
     elif c < 0:
-        while c >= 0:
+        while c > 0:
             a = cypher.pop()
             cypher.insert(a,0)
             c += 1
     else:
         return cypher
+    return cypher
 
 def caeser_cypher(letter, cypher):
     cypher_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
               "v", "w", "x", "y", "z"]
-    index = cypher_letters.index(letter)
-    new_letter = cypher[index]
+    try:
+        index = cypher_letters.index(letter)
+        new_letter = cypher[index]
+    except:
+        new_letter = letter
     return new_letter
 
 print(read_letters("letter.txt",1))
